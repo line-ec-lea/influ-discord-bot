79ce58ea3a82817dde570a5492f14c5f3943a693
Nakano as a Service <nakanoasaservice@gmail.com>
2025-02-19 11:59:33 +0900

    feat: Notionで開くボタンを追加

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Client as NotionClient } from "@notionhq/client";
 import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
-import type { RESTPostAPIChannelMessageJSONBody } from "discord-api-types/v10";
+import {
+	ButtonStyle,
+	ComponentType,
+	type RESTPostAPIChannelMessageJSONBody,
+} from "discord-api-types/v10";
 import { Hono } from "hono";
 import * as v from "valibot";
 import { formatProperty } from "./formatter";
@@ -100,13 +104,22 @@ app.post("/:discordChannelId", async (c) => {
 	).join("\n");
 
 	await sendDiscordMessage(env.DISCORD_BOT_TOKEN, discordChannelId, {
-		content: [
-			title,
-			formattedProperties || "[No properties to display]",
-			body.data.url,
-		]
+		content: [title, formattedProperties || "[No properties to display]"]
 			.filter(Boolean)
 			.join("\n"),
+		components: [
+			{
+				type: ComponentType.ActionRow,
+				components: [
+					{
+						type: ComponentType.Button,
+						label: "Notionで開く",
+						style: ButtonStyle.Link,
+						url: body.data.url,
+					},
+				],
+			},
+		],
 	});
 
 	return c.body(null, 204);
